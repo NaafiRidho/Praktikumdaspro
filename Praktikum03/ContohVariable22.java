feacd77780134982ac83406e6cf6545145a515dc
NaafiRidho <naafiridho0505@gmail.com>
2023-09-20 11:38:43 +0700

    perubahan 1

---
+++ Praktikum03/ContohVariable22.java
@@ -0,0 +1,17 @@
+public class ContohVariable22 {
+
+    public static void main(String[] args){
+
+        String salahSatuHobiSayaAdalah = "Bermain petak umpet";
+        boolean ispandai = true;
+        char jenisKelamin = 'L';
+        byte _umurSayaSekarang = 20;
+        double $ipk = 3.24, tinggi = 1.78;
+        
+        System.out.println("Hobi:" + salahSatuHobiSayaAdalah);
+        System.out.println("apakah pandai?" + ispandai);
+        System.out.println("jenisKelamin:" + jenisKelamin);
+        System.out.println("IPK" + $ipk);
+        System.out.println(String.format("saya berumur %s dengan tinggi badan %s", _umurSayaSekarang, tinggi));
+    }
+}
