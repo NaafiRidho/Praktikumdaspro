3b9fbbb49025eeaae5a00c16bc173e0a8dc16189
NaafiRidho <naafiridho0505@gmail.com>
2023-09-20 15:31:29 +0700

    percobaan 5

--- Praktikum03/Bank22.java
+++ Praktikum03/Bank22.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class Bank22 {
-    
+    public static void main(String[]args){
+
+    double tabunganAwal, tabunganAkhir, bunga, presentaseBunga;
+    int lamaMenabung;
+    Scanner sc = new Scanner(System.in);
+
+    presentaseBunga = 0.0002;
+    System.out.println("Masukkan jaumlah tabungan awal: ");
+    tabunganAwal = sc.nextDouble();
+    System.out.println("Madukkan lama menabung (bulan): ");
+    lamaMenabung = sc.nextInt();
+
+    bunga = presentaseBunga * lamaMenabung * tabunganAwal;
+    tabunganAkhir = tabunganAwal + bunga;
+
+    System.out.println("Bunga: " + bunga);
+    System.out.println("Tabungan akhir: " + tabunganAkhir);
+
+
+
+
+
+}
 }
