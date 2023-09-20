b7f2052f106811f3ac974eef12805e7babf09ec8
NaafiRidho <naafiridho0505@gmail.com>
2023-09-20 13:34:02 +0700

    percobaan 2

---
+++ Praktikum03/ContohTipeData22.java
@@ -0,0 +1,17 @@
+public class ContohTipeData22 {
+    public static void main(String[] args){
+        char golonganDarah = 'A';
+        Short jumlahPenduduk = 1025;
+        float suhu = 60.50F;
+        double berat = 0.5467812345;
+        long saldo = 150000000;
+        int angkaDesimal = 0x10;
+
+        System.out.println("Golongan\t: " + (byte)golonganDarah);
+        System.out.println("Jumlah Penduduk\t: " + jumlahPenduduk);
+        System.out.println("Suhu\t\t: " + suhu) ;
+        System.out.println("Berat\t\t: " + (float)berat);
+        System.out.println("Saldo\t\t: " + saldo);
+        System.out.println("Angka Desimal\t: " + angkaDesimal);
+    }
+}
--- Praktikum03/ContohVariable22.java
+++ Praktikum03/ContohVariable22.java
@@ -2,16 +2,18 @@ public class ContohVariable22 {
 
     public static void main(String[] args){
 
-        String salahSatuHobiSayaAdalah = "Bermain petak umpet";
-        boolean ispandai = true;
+        String nama = "naafi";
+        String hobi = "Bermain petak umpet";
+        boolean pandai = true;
         char jenisKelamin = 'L';
-        byte _umurSayaSekarang = 20;
-        double $ipk = 3.24, tinggi = 1.78;
-        
-        System.out.println("Hobi:" + salahSatuHobiSayaAdalah);
-        System.out.println("apakah pandai?" + ispandai);
+        byte umur = 20;
+        double ipk = 3.24, tinggi = 1.78;
+
+        System.out.println("Nama:" + nama);
+        System.out.println("Hobi:" + hobi);
+        System.out.println("apakah pandai?" + pandai);
         System.out.println("jenisKelamin:" + jenisKelamin);
-        System.out.println("IPK" + $ipk);
-        System.out.println(String.format("saya berumur %s dengan tinggi badan %s", _umurSayaSekarang, tinggi));
+        System.out.println("IPK:" + ipk);
+        System.out.println(String.format("saya berumur %s dengan tinggi badan %s", umur, tinggi));
     }
 }
