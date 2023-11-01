aef9d2c0664bb0b99d71bd9d3c160d089e9261e0
NaafiRidho <naafiridho0505@gmail.com>
2023-11-01 11:21:53 +0700

    percobaan 1

---
+++ Praktikum09/arraybilangan22.java
@@ -0,0 +1,9 @@
+public class arraybilangan22 {
+    public static void main(String[] args) {
+        int[]bilangan= {5,13,-7,17};
+
+      for(int i =0; i<=4; i++){
+        System.out.println(bilangan[i]);
+      }
+    }
+}
