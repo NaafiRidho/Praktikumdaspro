6ff3b1be530d61ddb794270b03dc5aef145cef0a
NaafiRidho <naafiridho0505@gmail.com>
2023-11-08 13:24:25 +0700

    menampilkan penonton pada baris ke 3

---
+++ Praktikum10/bioskop22.java
@@ -0,0 +1,29 @@
+package Praktikum10;
+
+public class bioskop22 {
+    public static void main(String[] args) {
+        String[][] penonton = new String[4][2];
+        penonton[0][0]= "amin";
+        penonton[0][1]= "bena";
+        penonton[1][0]= "candra";
+        penonton[1][1]= "dela";
+        penonton[2][0]= "eka";
+        penonton[2][1]= "farhan";
+        penonton[3][0]= "gisel";
+        penonton[3][1]= "hana";
+
+        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+
+        System.out.println(penonton.length);
+        for(String[] barisPenonton:penonton){
+            System.out.println("panjang baris " + barisPenonton.length);
+        }
+        System.out.println("penonton pada baris ke-3: ");
+        for(int i = 0; i<penonton[2].length; i++){
+            System.out.println(penonton[2][i]);
+        }
+    }
+}
